0ebd0e64bc0961bb40a835b9bb65c98b2239c4a2
torbastian <torbastianhansen@hotmail.com>
2021-03-10 08:43:56 +0100

    Card Model Add

---
+++ KNBN API/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KNBN_API.Models
+{
+    public class Card
+    {
+        [Key]
+        public int CardID { get; set; }
+
+        public Table Table { get; set; }
+        [ForeignKey("Table")]
+        public int TableID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
