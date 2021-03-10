839c36366417ada13e8faf1f1bf6a92d1b266a2b
torbastian <torbastianhansen@hotmail.com>
2021-03-10 07:58:05 +0100

    Board Model Add

---
+++ KNBN API/Models/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace KNBN_API.Models
+{
+    public class Board
+    {
+        [Key]
+        public int BoardID { get; set; }
+
+        //Foreign Key -> User
+        public List<User> UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
