0f5c05bb86e0bc14c0598a847b4e6dc46948ce5b
Tino Pilbak Snellman Floris <tino0100@elev.tec.dk>
2021-03-10 08:35:34 +0100

    Create User_info.cs

---
+++ KNBN API/Models/User_info.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNBN_API.Models
+{
+    public class User_info
+    {
+        [Key]
+        public int Email { get; set; }
+
+        public User User { get; set; }
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Color { get; set; }
+
+
+
+    }
+}
