9155133b6a0e6a34663033d25af0fed50456fe52
sami0596 <gamesteamdk@hotmail.com>
2021-03-10 08:29:14 +0100

    Table added

---
+++ KNBN API/Models/Table.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+public class Table
+{
+    [Key]
+    public int TableID { get; set; }
+
+    //Foreign Key -> User
+    public Table Table { get; set; }
+    [ForeignKey("Table")]
+    public int BoardID { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+}
