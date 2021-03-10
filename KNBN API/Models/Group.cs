cc9fd0cf0f42204b2a74943811684159b61c1c0d
tomodo531 <tomodo531@gmail.com>
2021-03-10 08:07:36 +0100

    Group model

---
+++ KNBN API/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KNBN_API.Models
+{
+    public class Group
+    {
+        [Key]
+        public int GroupID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Color { get; set; }
+    }
+}
