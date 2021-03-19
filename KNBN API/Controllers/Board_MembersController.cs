723fb0d830b2a4657b958e54a6ff9920d634ddec
torbastian <torbastianhansen@hotmail.com>
2021-03-19 08:04:13 +0100

    Scaffold

---
+++ KNBN API/Controllers/Board_MembersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KNBN_API.Models;
+
+namespace KNBN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Board_MembersController : ControllerBase
+    {
+        private readonly KanbanContext _context;
+
+        public Board_MembersController(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Board_Members
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Board_Members>>> GetBoard_Members()
+        {
+            return await _context.Board_Members.ToListAsync();
+        }
+
+        // GET: api/Board_Members/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Board_Members>> GetBoard_Members(int id)
+        {
+            var board_Members = await _context.Board_Members.FindAsync(id);
+
+            if (board_Members == null)
+            {
+                return NotFound();
+            }
+
+            return board_Members;
+        }
+
+        // PUT: api/Board_Members/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBoard_Members(int id, Board_Members board_Members)
+        {
+            if (id != board_Members.Board_MembersId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(board_Members).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Board_MembersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Board_Members
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Board_Members>> PostBoard_Members(Board_Members board_Members)
+        {
+            _context.Board_Members.Add(board_Members);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBoard_Members", new { id = board_Members.Board_MembersId }, board_Members);
+        }
+
+        // DELETE: api/Board_Members/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBoard_Members(int id)
+        {
+            var board_Members = await _context.Board_Members.FindAsync(id);
+            if (board_Members == null)
+            {
+                return NotFound();
+            }
+
+            _context.Board_Members.Remove(board_Members);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool Board_MembersExists(int id)
+        {
+            return _context.Board_Members.Any(e => e.Board_MembersId == id);
+        }
+    }
+}
---
+++ KNBN API/Controllers/BoardsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KNBN_API.Models;
+
+namespace KNBN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        private readonly KanbanContext _context;
+
+        public BoardsController(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Boards
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Board>>> GetBoards()
+        {
+            return await _context.Boards.ToListAsync();
+        }
+
+        // GET: api/Boards/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Board>> GetBoard(int id)
+        {
+            var board = await _context.Boards.FindAsync(id);
+
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return board;
+        }
+
+        // PUT: api/Boards/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBoard(int id, Board board)
+        {
+            if (id != board.BoardId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(board).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BoardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Boards
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Board>> PostBoard(Board board)
+        {
+            _context.Boards.Add(board);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBoard", new { id = board.BoardId }, board);
+        }
+
+        // DELETE: api/Boards/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBoard(int id)
+        {
+            var board = await _context.Boards.FindAsync(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            _context.Boards.Remove(board);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BoardExists(int id)
+        {
+            return _context.Boards.Any(e => e.BoardId == id);
+        }
+    }
+}
