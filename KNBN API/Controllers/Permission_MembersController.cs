1ff5687b3c6bc65fd91174171cafdf41aad53d9b
Hao <54834780+HaoYeChen@users.noreply.github.com>
2021-03-19 08:06:29 +0100

    scaffolding
    User, User info, Permission members, permission Controllers

---
+++ KNBN API/Controllers/Permission_MembersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KNBN_API.Models;
+
+namespace KNBN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Permission_MembersController : ControllerBase
+    {
+        private readonly KanbanContext _context;
+
+        public Permission_MembersController(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Permission_Members
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Permission_Members>>> GetPermission_Members()
+        {
+            return await _context.Permission_Members.ToListAsync();
+        }
+
+        // GET: api/Permission_Members/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Permission_Members>> GetPermission_Members(int id)
+        {
+            var permission_Members = await _context.Permission_Members.FindAsync(id);
+
+            if (permission_Members == null)
+            {
+                return NotFound();
+            }
+
+            return permission_Members;
+        }
+
+        // PUT: api/Permission_Members/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPermission_Members(int id, Permission_Members permission_Members)
+        {
+            if (id != permission_Members.Permission_MemebersId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(permission_Members).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Permission_MembersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Permission_Members
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Permission_Members>> PostPermission_Members(Permission_Members permission_Members)
+        {
+            _context.Permission_Members.Add(permission_Members);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPermission_Members", new { id = permission_Members.Permission_MemebersId }, permission_Members);
+        }
+
+        // DELETE: api/Permission_Members/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePermission_Members(int id)
+        {
+            var permission_Members = await _context.Permission_Members.FindAsync(id);
+            if (permission_Members == null)
+            {
+                return NotFound();
+            }
+
+            _context.Permission_Members.Remove(permission_Members);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool Permission_MembersExists(int id)
+        {
+            return _context.Permission_Members.Any(e => e.Permission_MemebersId == id);
+        }
+    }
+}
---
+++ KNBN API/Controllers/PermissionsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KNBN_API.Models;
+
+namespace KNBN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionsController : ControllerBase
+    {
+        private readonly KanbanContext _context;
+
+        public PermissionsController(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Permissions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Permissions>>> GetPermissions()
+        {
+            return await _context.Permissions.ToListAsync();
+        }
+
+        // GET: api/Permissions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Permissions>> GetPermissions(int id)
+        {
+            var permissions = await _context.Permissions.FindAsync(id);
+
+            if (permissions == null)
+            {
+                return NotFound();
+            }
+
+            return permissions;
+        }
+
+        // PUT: api/Permissions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPermissions(int id, Permissions permissions)
+        {
+            if (id != permissions.PermissionsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(permissions).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PermissionsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Permissions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Permissions>> PostPermissions(Permissions permissions)
+        {
+            _context.Permissions.Add(permissions);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPermissions", new { id = permissions.PermissionsId }, permissions);
+        }
+
+        // DELETE: api/Permissions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePermissions(int id)
+        {
+            var permissions = await _context.Permissions.FindAsync(id);
+            if (permissions == null)
+            {
+                return NotFound();
+            }
+
+            _context.Permissions.Remove(permissions);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PermissionsExists(int id)
+        {
+            return _context.Permissions.Any(e => e.PermissionsId == id);
+        }
+    }
+}
